17436a54dbea34ce4b7ae1cb37dacc297f6caa04
Diablo <info@m.demoneum.com>
2020-09-02 02:32:08 +0200

    Add inventory api models.

---
+++ lib/models/document.go
@@ -0,0 +1,28 @@
+package models
+
+type Document struct {
+}
+
+type DocumentType struct {
+}
+
+type DocumentField struct {
+}
+
+type DocumentFieldValue struct {
+}
+
+type DocumentRelation struct {
+}
+
+type DocumentRelationType struct {
+}
+
+type DocumentRelationValue struct {
+}
+
+type DocumentMutation struct {
+}
+
+type DocumentFieldType struct {
+}
