9fae30d5d5f43eb5267cfbe1a6a28027d67968ab
Diablo <info@m.demoneum.com>
2020-09-09 01:42:49 +0200

    Starting to add the actual workspace selection.

--- workspace/src/constants/env.ts
+++ workspace/src/constants/env.ts
@@ -10,5 +10,11 @@ export const AUTH_API_URL =
     ? "??"
     : "http://localhost:3333"
 
+export const WORKSPACE_API_URL =
+  !DEV
+    ? "??"
+    : "http://localhost:3334"
+
 export const ENDPOINT_AUTH_URL = `${AUTH_API_URL}/auth`
 export const ENDPOINT_LOGIN_URL = `${AUTH_API_URL}/login`
+export const ENDPOINT_WORKSPACES_URL = `${WORKSPACE_API_URL}/user/workspaces`
---
+++ workspace/src/lib/workspace.ts
@@ -0,0 +1,35 @@
+import protect from "await-protect"
+import axios from "axios"
+import { ENDPOINT_WORKSPACES_URL } from "../constants/env"
+
+export interface Workspace {
+  id: string
+  name: string
+  active: boolean
+  users: null
+}
+
+interface WorkspacesResponse {
+  workspaces: Workspace[]
+}
+
+interface WorkspacesResult {
+  success: boolean
+  error: string
+  workspaces: Workspace[]
+}
+
+export async function getWorkspaces(): Promise<WorkspacesResult> {
+  const [res, error] = await protect(
+    axios.get(ENDPOINT_WORKSPACES_URL, { withCredentials: true })
+  )
+
+  if (error) {
+    return { success: false, error: error.message, workspaces: []}
+  }
+
+  const body: WorkspacesResponse = res.data
+  const workspaces = body.workspaces
+
+  return { success: true, workspaces, error: "" }
+}
