813d993732b7dbb15ad3ef753a04b9d793d92d75
Diablo <info@m.demoneum.com>
2021-02-14 18:48:04 +0100

    .

--- auth/internal/api/handlers.go
+++ auth/internal/api/handlers.go
@@ -53,7 +53,7 @@ func (api *API) AddHandlers(s *archhttp.EchoServer) {
 		SigningKey:    signingKey,
 		SigningMethod: jwt.SigningMethodRS256.Name,
 		ErrorHandlerWithContext: func(err error, c echo.Context) error {
-			api.logger.Debugw("Unable to verify token", "err", err.Error())
+			api.logger.Debugw("Unable to verify token", "errMessage", err.Error())
 
 			return c.JSON(http.StatusUnauthorized, echo.Map{"message": "token is invalid"})
 		},
