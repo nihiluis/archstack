0e24c560f8b8d90dcbaca076a7cebe5bc9a189eb
Diablo <info@m.demoneum.com>
2021-02-14 19:09:19 +0100

    Add debug logging to cookie middleware.

--- go.mod
+++ go.mod
@@ -5,8 +5,9 @@ go 1.14
 require (
 	github.com/dghubble/sling v1.3.0
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
-	github.com/gofrs/uuid v3.3.0+incompatible
-	github.com/gorilla/sessions v1.2.1
-	github.com/labstack/echo/v4 v4.1.17
-	go.uber.org/zap v1.15.0
+	github.com/go-pg/pg/v10 v10.7.6
+	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/labstack/echo/v4 v4.2.0
+	go.uber.org/zap v1.16.0
+	github.com/joho/godotenv v1.3.0
 )
--- lib/server/http/middleware/cookie.go
+++ lib/server/http/middleware/cookie.go
@@ -2,22 +2,28 @@ package middleware
 
 import (
 	"github.com/labstack/echo/v4"
+	"gitlab.com/archstack/core-api/lib/logger"
 )
 
-func UserCookieAuth() echo.MiddlewareFunc {
+func UserCookieAuth(logger *logger.Logger) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			req := c.Request()
 
 			if req.Header.Get("Authorization") != "" {
+				logger.Zap.Debugw("found Authorization header")
 				return next(c)
 			}
 
 			cookie, err := c.Cookie("token")
 
 			if err != nil || cookie.Value == "" {
+				logger.Zap.Debugw("no cookie found in request.", "err", err.Error())
+
 				return next(c)
 			}
+
+			logger.Zap.Debugw("found cookie token in request", "token", cookie.Value)
 
 			token := cookie.Value
 
