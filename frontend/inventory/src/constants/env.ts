eb239abbab15076f4a6aac4e5801703ba3ad966c
Diablo <info@m.demoneum.com>
2021-02-15 21:40:29 +0100

    Update base path.

--- frontend/inventory/src/constants/env.ts
+++ frontend/inventory/src/constants/env.ts
@@ -2,7 +2,7 @@ export const DEV = process.env.NODE_ENV !== "production"
 
 export const IS_SERVER = typeof window === "undefined"
 
-export const BASE_PATH = "/app"
+export const BASE_PATH = "/inventory-app"
 
 export const SITE_TITLE = "IT repo"
 export const PRODUCT_NAME = "IT repo"
